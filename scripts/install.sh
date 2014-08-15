664c826e04d258e9446b09ed66d1c9437add330f
Konstantinos Vandikas <konstantinos.vandikas@ericsson.com>
2014-08-15 12:30:18 +0200

    Fixed esl-erlang dependencies and updated to trusty repo for R

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 ### Author : Tommy Mattsson
 ### Purpose: Easy installation of needed software for our project that is part of the linux system rather than our own project
+
+#Break on error
+set -e
+
 echo "#################################################################"
 echo "Installing misc dependencies"
 echo "#################################################################"
-sudo apt-get install -yq xsltproc software-properties-common
+sudo apt-get install -yq xsltproc software-properties-common 
 
 echo "#################################################################"
-echo "Installing Erlang"
+echo "Installing openjdk-7"
+echo "#################################################################"
+sudo apt-get install -yq openjdk-7-jre-headless libwxgtk2.8-0
+
+echo "#################################################################"
+echo "Installing Erlang 16B1"
 echo "#################################################################"
 wget http://packages.erlang-solutions.com/site/esl/esl-erlang/FLAVOUR_1_general/esl-erlang_16.b.1~ubuntu~precise_amd64.deb
 sudo dpkg -i esl-erlang_16.b.1~ubuntu~precise_amd64.deb
@@ -24,6 +33,6 @@ sudo apt-get install -yq nodejs
 echo "#################################################################"
 echo "Installing R"
 echo "#################################################################"
-sudo add-apt-repository "deb http://ftp.sunet.se/pub/lang/CRAN/bin/linux/ubuntu precise/"
+sudo add-apt-repository "deb http://ftp.sunet.se/pub/lang/CRAN/bin/linux/ubuntu trusty/"
 sudo apt-get update -q
-sudo apt-get install -yq r-base
+sudo apt-get install \r-base
